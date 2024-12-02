900b15aa8cee280fdcd2b113f4e23f2510bd755c
Gellert <stgellertg@gmail.com>
2024-12-02 18:49:27 +0100

    update training script and minimax team

--- run_games_quiet.sh
+++ run_games_quiet.sh
@@ -1,7 +1,7 @@
-i=(0)
-while :
-do
-    python3 capture.py --delay 0.0 -r baseline_team -b ../../../my_team.py -l RANDOM --quiet
+i=0
+while true; do
+    echo -n .
+    python3 capture.py --delay 0.0 -r ../../../my_minimax_team.py -b ../../../my_team.py -l RANDOM --quiet
     i=$((i+1))
     echo $i
 done
